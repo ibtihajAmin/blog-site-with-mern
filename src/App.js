329df5ec5e9e8867bb451365b4d53660a4d1cd88
ibtihajAmin <ibtihaj.amin18@gmail.com>
2023-05-07 10:22:58 +0600

    Basic Routing is added

--- src/App.js
+++ src/App.js
@@ -1,97 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="http://localhost:3000/" className="logo">
-          My Blog
-        </a>
-        <nav>
-          <a href="http://localhost:3000/">Login</a>
-          <a href="http://localhost:3000/">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2021/11/facebook-meta-rotate-pattern.jpg?w=1390&crop=1"
-            alt=""
-          />
-        </div>
-
-        <div className="texts">
-          <h2>
-            Hacked verified Facebook pages impersonating Meta are buying ads
-            from Meta
-          </h2>
-          <p className="info">
-            <a href="http://localhost:3000/" className="author">
-              David Malan
-            </a>
-            <time>2023-05-06 21:40</time>
-          </p>
-          <p className="summary">
-            A handful of verified Facebook pages were hacked recently and
-            spotted slinging likely malware through ads approved by and
-            purchased through the platform. But the accounts should be easy to
-            catch — in some cases, they were impersonating Facebook itself.
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2021/11/facebook-meta-rotate-pattern.jpg?w=1390&crop=1"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>
-            Hacked verified Facebook pages impersonating Meta are buying ads
-            from Meta
-          </h2>
-          <p className="info">
-            <a href="http://localhost:3000/" className="author">
-              David Vila
-            </a>
-            <time>2023-05-06 21:40</time>
-          </p>
-          <p className="summary">
-            A handful of verified Facebook pages were hacked recently and
-            spotted slinging likely malware through ads approved by and
-            purchased through the platform. But the accounts should be easy to
-            catch — in some cases, they were impersonating Facebook itself.
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2021/11/facebook-meta-rotate-pattern.jpg?w=1390&crop=1"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>
-            Hacked verified Facebook pages impersonating Meta are buying ads
-            from Meta
-          </h2>
-          <p className="info">
-            <a href="http://localhost:3000/" className="author">
-              Jason Malan
-            </a>
-            <time>2023-05-06 21:40</time>
-          </p>
-          <p className="summary">
-            A handful of verified Facebook pages were hacked recently and
-            spotted slinging likely malware through ads approved by and
-            purchased through the platform. But the accounts should be easy to
-            catch — in some cases, they were impersonating Facebook itself.
-          </p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={"/login"} element={<LoginPage />}></Route>
+      </Route>
+    </Routes>
   );
 }
 
