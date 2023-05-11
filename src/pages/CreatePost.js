854dfcf851797adc2a5dfdd4c52427f3e63dbcde
ibtihajAmin <ibtihaj.amin18@gmail.com>
2023-05-11 14:19:02 +0600

    CreatePost UI is added

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -33,9 +33,23 @@ export default function CreatePost() {
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+
+  function createNewPost(e) {
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("file", files[0]);
+    e.preventDefault();
+    fetch("http://localhost:4000/post", {
+      method: "POST",
+      body: data,
+    });
+  }
 
   return (
-    <form>
+    <form onSubmit={createNewPost}>
       <input
         type="tile"
         placeholder="Title"
@@ -48,7 +62,7 @@ export default function CreatePost() {
         value={summary}
         onChange={(e) => setSummary(e.target.value)}
       />
-      <input type="file" />
+      <input type="file" onChange={(e) => setFiles(e.target.files)} />
       <ReactQuill
         value={content}
         modules={modules}
