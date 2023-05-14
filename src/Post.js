316fe4f986639a400b84244b90de68ac8ef152ea
ibtihajAmin <ibtihaj.amin18@gmail.com>
2023-05-14 20:30:41 +0600

    Post Shown is added

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,5 @@
-export default function Post() {
+import { format } from "date-fns";
+export default function Post({ title, summary, cover, content, createdAt }) {
   return (
     <div className="post">
       <div className="image">
@@ -9,22 +10,14 @@ export default function Post() {
       </div>
 
       <div className="texts">
-        <h2>
-          Hacked verified Facebook pages impersonating Meta are buying ads from
-          Meta
-        </h2>
+        <h2>{title}</h2>
         <p className="info">
           <a href="http://localhost:3000/" className="author">
             David Malan
           </a>
-          <time>2023-05-06 21:40</time>
+          <time>{format(new Date(createdAt), "MMM d, yyyy HH:mm")}</time>
         </p>
-        <p className="summary">
-          A handful of verified Facebook pages were hacked recently and spotted
-          slinging likely malware through ads approved by and purchased through
-          the platform. But the accounts should be easy to catch — in some
-          cases, they were impersonating Facebook itself.
-        </p>
+        <p className="summary">{summary}</p>
       </div>
     </div>
   );
